01b88be13090d62a41373717b79f2e3e302293ad
fioresat <104829704+fioresat@users.noreply.github.com>
2022-06-15 23:17:14 +0300

    Update consts.dart

--- lib/data/consts.dart
+++ lib/data/consts.dart
@@ -1,6 +1,6 @@
 abstract class Consts {
   static const baseUrl = 'https://api.giphy.com/v1/gifs/'; //Kong Error
-  static const openGifMapKey = '6gzl4hD59JeoMuLCWSdJ7wzWeKP2P3aQ'; // API KEY
+  static const openGifMapKey = 'API KEY HERE PLEASE'; // API KEY
   static const getGifRequest = 'search'; //Request
   static const gifLimit = 12;
 //https://api.giphy.com/v1/gifs/search?api_key=6gzl4hD59JeoMuLCWSdJ7wzWeKP2P3aQ&q=cats&limit=12&offset=0&rating=g&lang=en
